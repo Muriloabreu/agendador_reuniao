65195ccc8301773ad29ad4ae612cac9689c6ab7e
muriloabreu <murilo.lilo@hotmail.com>
2023-03-30 18:07:11 -0300

    Criação tela HTML Listagem de Reservas de Reuniões

--- src/main/java/com/app/agenda_reuniao/controller/EvetoController.java
+++ src/main/java/com/app/agenda_reuniao/controller/EvetoController.java
@@ -3,20 +3,23 @@ package com.app.agenda_reuniao.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.app.agenda_reuniao.models.EventoModel;
 import com.app.agenda_reuniao.service.EventoService;
 
+@Controller
 public class EvetoController {
 	
 	@Autowired
 	EventoService eventoService;
 
-	@RequestMapping(value = "/agendareuniao")
+	@RequestMapping(value = "/agendar", method = RequestMethod.GET)
 	public ModelAndView getEventos() {
-		ModelAndView mv = new ModelAndView("eventos");
+		ModelAndView mv = new ModelAndView("agendareuniao"); /*Nome da Pagina html*/
 		List<EventoModel> eventos = eventoService.findAll();
 		mv.addObject("eventos", eventos);
 		return mv;
