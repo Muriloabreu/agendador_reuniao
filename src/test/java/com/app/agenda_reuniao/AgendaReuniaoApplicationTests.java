50003f4f78dc632d097501f1af80c2b74dbef866
Murilo Abreu <murilo.lilo@hotmail.com>
2026-06-22 22:00:31 -0300

    Limpando arquivo, mas depois será refeito

--- src/test/java/com/app/agenda_reuniao/AgendaReuniaoApplicationTests.java
+++ src/test/java/com/app/agenda_reuniao/AgendaReuniaoApplicationTests.java
@@ -1,12 +1,12 @@
 package com.app.agenda_reuniao;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AgendaReuniaoApplicationTests {
 
-	@Test
+	
 	void contextLoads() {
 	}
 
